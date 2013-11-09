d974c15ebcd5380f27a1303ce34e8f9ac369d221
lucas <lucas1000001@gmail.com>
2013-11-09 22:03:42 +0000

    All but one passes

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -6,6 +6,7 @@ import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+import scala.annotation.tailrec
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -14,7 +15,58 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("del one is empty") = forAll { a:Int =>
+    val one = insert(a, empty)
+    all {
+      deleteMin(one) == empty
+    }
+  }
+
+  property("min returned") = forAll { (a:Int, b:Int) =>
+    (a < b) ==> {
+      val h1 = insert(b, insert(a, empty))
+      val h2 = insert(a, insert(b, empty))
+      all(
+        findMin(h1) == a,
+        findMin(h2) == a
+      )
+    }
+  }
+
+  property("del min") = forAll { (a:Int, b:Int) =>
+    (a < b) ==> {
+      val h1 = insert(b, insert(a, empty))
+      val h2 = insert(a, insert(b, empty))
+      val hb = insert(b, empty)
+      all(
+        deleteMin(h1) == hb,
+        deleteMin(h2) == hb
+      )
+    }
+  }
+
+  @tailrec private def asSeq(h:H, sorted:List[Int]):List[Int] = {
+    h match {
+      case h if h == empty => sorted
+      case h => asSeq(deleteMin(h), sorted :+ findMin(h))
+    }
+  }
+
+  property("returns sorted seq") = forAll { (h:H) =>
+    val seq = asSeq(h, List())
+    seq == seq.sorted
+  }
+
+  property("able to meld heaps") = forAll { (h1:H, h2:H) =>
+    val min = List(findMin(h1), findMin(h2)).min
+    findMin(meld(h1, h2)) == min
+  }
+
+
+  lazy val genHeap: Gen[H] = for {
+    v <- arbitrary[Int]
+    h <- oneOf(value(empty), genHeap)
+  } yield insert(v, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
