eb4ef428793f29a8c2de2d028ccaac58a375c81d
lucas <lucas1000001@gmail.com>
2013-11-19 07:21:25 +0000

    Attempt one finished

--- simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -1,10 +1,17 @@
 package simulations
 
 import math.random
+import scala.util.Random
 
 class EpidemySimulator extends Simulator {
 
   def randomBelow(i: Int) = (random * i).toInt
+
+  def randomBelowNonZero(i:Int) = randomBelow(i) + 1
+
+  def randomSampleOf[A](n:Int, ns:List[A]) = {
+    Random.shuffle(ns).take(n)
+  }
 
   protected[simulations] object SimConfig {
     val population: Int = 300
@@ -12,13 +19,92 @@ class EpidemySimulator extends Simulator {
     val roomColumns: Int = 8
 
     // to complete: additional parameters of simulation
+    val prevalenceRate = 0.01
+    val moveDelay = 5
+    val incubationDelay = 6
+    val deathsDoorDelay = 14 - incubationDelay
+    val immunityDelay = 16 - deathsDoorDelay - incubationDelay
+    val healthyDelay = 18 - immunityDelay - deathsDoorDelay - incubationDelay
+
+    val canFly = false
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val rooms = new Rooms(roomRows, roomColumns)
 
-  class Person (val id: Int) {
+  val persons: List[Person] = (0 until population).map(new Person(_, rooms)).toList
+  randomSampleOf((prevalenceRate * population).toInt, persons).foreach(_.caught())
+
+  /**
+   * Direction enum to enumerate the four directions
+   */
+  object Direction extends Enumeration {
+
+    case class Direction(r:Int, c:Int)
+
+    val N = Direction(-1, 0)
+    val S = Direction(1, 0)
+    val E = Direction(0, 1)
+    val W = Direction(0, -1)
+
+    private val bag = List(N, S, E, W)
+
+    def random() = {
+      Random.shuffle(bag).head
+    }
+
+    def randomMove(coord:(Int, Int)) = {
+      val (row, col) = coord
+      val Direction(rowInc, colInc) = random()
+      val newRow = ((row + rowInc + roomRows) % roomRows)
+      val newCol = ((col + colInc + roomColumns) % roomColumns)
+      (newRow, newCol)
+    }
+
+    def randomFlight() = {
+      val newRow = randomBelow(roomRows)
+      val newCol = randomBelow(roomColumns)
+      (newRow, newCol)
+    }
+
+  }
+
+
+  class Rooms(val rows:Int, val cols:Int) {
+
+    import scala.collection.mutable
+
+    private val rooms = for (row <- 0 until rows) yield {
+      for (col <- 0 until cols) yield { mutable.Set[Person]() }
+    }
+
+    def room(coord:(Int, Int)) = {
+      val (r, c) = coord
+      rooms(r)(c)
+    }
+
+    def join(coord:(Int, Int), person:Person) {
+      room(coord) += person
+    }
+
+    def leave(coord:(Int, Int), person:Person) {
+      room(coord) -= person
+    }
+
+    def hasVisiblyInfected(coord:(Int, Int)) = !isSafe(coord)
+    
+    def hasInfected(coord:(Int, Int)) = room(coord).foldLeft(false) { (anyInfected, person) => anyInfected || person.infected }
+
+    def isSafe(coord:(Int, Int)) = room(coord).forall { p => !p.dead || !p.sick }
+
+  }
+
+  def P(p:Int) = {
+    randomBelow(100) < p
+  }
+
+  class Person (val id: Int, val rooms:Rooms) {
     var infected = false
     var sick = false
     var immune = false
@@ -28,8 +114,104 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
+    //schedule a move on start
+    move()
+
     //
     // to complete with simulation logic
     //
+
+
+    import Direction._
+
+    def leave() {
+      rooms.leave((row, col), this)
+    }
+
+    def becomesHealthy() {
+      afterDelay(healthyDelay) {
+        immune = false
+        infected = false
+      }
+    }
+
+    def becomesImmune() {
+      afterDelay(immunityDelay) {
+        sick = false
+        immune = true
+        becomesHealthy()
+      }
+    }
+
+    def chanceOfDeath() {
+      afterDelay(deathsDoorDelay) {
+        val dies = P(25)
+        if (dies) {
+          dead = true
+        } else {
+          becomesImmune()
+        }
+      }
+    }
+
+    def becomesSick() {
+      afterDelay(incubationDelay) {
+        sick = true
+        chanceOfDeath()
+      }
+    }
+
+    def caught() {
+      infected = true
+      becomesSick()
+    }
+
+    def exposed() {
+      val caughtIt = P(40)
+      if (caughtIt && !infected && !immune)
+        caught()
+    }
+
+    def join() {
+      rooms.join((row, col), this)
+      if (rooms.hasInfected((row, col)))
+        exposed()
+    }
+
+    def walk() {
+      val (r, c) = randomMove((row, col))
+      if (rooms.isSafe((r, c))) {
+        row = r
+        col = c
+      }
+    }
+
+    def fly() {
+      val (r, c) = randomFlight()
+      row = r
+      col = c
+    }
+
+    def choose() {
+      val takeFlight = P(1) && canFly
+      if (takeFlight) {
+        fly()
+      } else {
+        walk()
+      }
+    }
+
+    def move() {
+      val delay = randomBelowNonZero(moveDelay)
+      afterDelay(delay) {
+        if (!dead) {
+          leave()
+          choose()
+          join()
+          move()
+        }
+      }
+    }
+
   }
 }
--- simulations/src/test/scala/simulations/EpidemySuite.scala
+++ simulations/src/test/scala/simulations/EpidemySuite.scala
@@ -8,6 +8,32 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class EpidemySuite extends FunSuite {
 
+  test("random direction") {
+    val es = new EpidemySimulator
+    import es.Direction._
+    val d = random()
+    assert (List(N, S, E, W).contains(d))
+  }
+
+  test("random room") {
+    val es = new EpidemySimulator
+    val room = es.Direction.randomMove((3, 3))
+    assert (List((2, 3), (4, 3), (3, 2), (3, 4)).contains(room))
+  }
+
+  test("random room lower edge") {
+    val es = new EpidemySimulator
+    val room = es.Direction.randomMove((0, 0))
+    println(room)
+    assert (List((7, 0), (1, 0), (0, 7), (0, 1)).contains(room))
+  }
+
+  test("random room upper edge") {
+    val es = new EpidemySimulator
+    val room = es.Direction.randomMove((7, 7))
+    assert (List((7, 0), (7, 6), (0, 7), (6, 7)).contains(room))
+  }
+
   test("prevalence rate"){
     val prevalenceRate = 0.01
 
@@ -82,6 +108,53 @@ class EpidemySuite extends FunSuite {
 
     assert(es.agenda.head.time == dieTime, "You should set a 'die' event (decides with a probability 25% whether the person dies) after 14 days")
     while(es.agenda.head.time == dieTime) es.next
+
+    val deadPerson = (es.persons.find(_.dead)).get
+    val sickPerson = (es.persons.find(_.sick)).get
+
+    // wait for immune time
+    while(es.agenda.head.time < immuneTime){
+      assert(deadPerson.infected == true, "Dead person keeps infected")
+      assert(deadPerson.sick == true, "Dead person keeps sick before turning immune")
+      assert(deadPerson.immune == false, "Dead person is not immune")
+      assert(deadPerson.dead == true, "Dead person does not die before 14 infected days")
+      assert(sickPerson.infected == true, "Sick person keeps infected")
+      assert(sickPerson.sick == true, "Sick person keeps sick before turning immune")
+      assert(sickPerson.immune == false, "Sick person is not immune")
+      assert(sickPerson.dead == false, "Sick person does not die before 14 infected days")
+      es.next
+    }
+
+    assert(es.agenda.head.time == immuneTime, "You should set an 'immune' event (decides with a probability 25% whether the person dies) after 14 days")
+    while(es.agenda.head.time == immuneTime) es.next
+
+
+    while(es.agenda.head.time < healTime){
+      assert(deadPerson.infected == true, "Dead person keeps infected")
+      assert(deadPerson.sick == true, "Dead person keeps sick before turning immune")
+      assert(deadPerson.immune == false, "Dead person is not immune")
+      assert(deadPerson.dead == true, "Dead person does not die before 14 infected days")
+      assert(sickPerson.infected == true, "Immune person keeps infected")
+      assert(sickPerson.sick == false, "Immune person keeps sick before turning immune")
+      assert(sickPerson.immune == true, "Immune person is immune")
+      assert(sickPerson.dead == false, "Immune person does not die before 14 infected days")
+      es.next
+    }
+
+    assert(es.agenda.head.time == healTime, "You should set a 'heal' event (decides with a probability 25% whether the person dies) after 14 days")
+    while(es.agenda.head.time == healTime) es.next
+
+    // Back to start
+    assert(deadPerson.infected == true, "Dead person keeps infected")
+    assert(deadPerson.sick == true, "Dead person keeps sick before turning immune")
+    assert(deadPerson.immune == false, "Dead person is not immune")
+    assert(deadPerson.dead == true, "Dead person does not die before 14 infected days")
+    assert(sickPerson.infected == false, "Immune person not infected")
+    assert(sickPerson.sick == false, "Immune person not sich")
+    assert(sickPerson.immune == false, "Immune person is not immune")
+    assert(sickPerson.dead == false, "Immune person does not die before 14 infected days")
+
+
   }
 
 
